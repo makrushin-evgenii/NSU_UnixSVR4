fa27467fac777cac71b4022ce20b21a495e2cbe6
kotbauk <kotbauk@gmail.com>
2018-05-01 19:40:54 +0700

    third lab zaybok editon

--- 3/3.c
+++ 3/3.c
@@ -1,55 +1,55 @@
 #include <errno.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
 
-#define FOPEN_FAIL 2
+#define DEFAULT_FILE_NAME "file"
 
-int main(int argc, char *argv[])
-{
+//Open file wrapper
+FILE* open_file(const char* file_name);
+
+int main(int argc, char* argv[]) {
     // Платиновые ответы:
 
     // Значение UID ставится в соответствие пользователю в файле /etc/passwd
 
-    // Операционная система различает пользователей именно по UID (а не, например, по логину).
-    // Во многих системах существует возможность создать две записи пользователя с разными логинами,
+    // Операционная система различает пользователей именно по UID (а не,
+    // например, по логину).
+    // Во многих системах существует возможность создать две записи пользователя
+    // с разными логинами,
     // но одинаковыми UID; в результате оба логина будут иметь одинаковые права,
-    // так как с точки зрения системы они неотличимы (так как имеют одинаковый UID).
+    // так как с точки зрения системы они неотличимы (так как имеют одинаковый
+    // UID).
 
     // Суперпользователь всегда должен иметь UID, равный нулю (0)
 
     // UIDы с 1 по 100 по соглашению резервируются под системные нужды
 
     // Почему не могу использовать скрипт для демонстрации:
-    // Due to the increased likelihood of security flaws, many operating systems ignore the setuid attribute when applied to executable shell scripts.
+    // Due to the increased likelihood of security flaws, many operating systems
+    // ignore the setuid attribute when applied to executable shell scripts.
 
-    FILE *file;
+    FILE* input_file;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s [file]\n", argv[0]);
-        return EINVAL;
-    }
+    // If the input file name wasn't entered, use default file name.
+    const char* file_name = (argc == 2) ? argv[1] : DEFAULT_FILE_NAME;
 
-    for (int i = 0; i < 2; ++i)
-    {
-        printf("uid=%d, euid=%d\n", getuid(), geteuid());
-
-        if ((file = fopen(argv[1], "r")) == NULL)
-        {
-            printf("%d try: access denied\n", i + 1);
-            perror(argv[0]);
-            return FOPEN_FAIL;
+    for (int index = 0; index < 2; ++index) {
+        fprintf(stdout, "Real UID:%u\nEffective UID:%u\n", getuid(), geteuid());
+        input_file = open_file(file_name);
+        if (input_file == NULL) {
+            return (EXIT_FAILURE);
         }
-        printf("%d try: accessed\n", i + 1);
-        fclose(file);
-
-        if (setuid(geteuid()) < 0)
-        {
-            fprintf(stderr, "cant change euid\n");
+        if (setuid(getuid()) < 0) {
+            fputs("Can't change euid\n", stderr);
             perror(argv[0]);
-            return EINVAL;
+            return (EXIT_FAILURE);
+        }
+
+        if (fclose(input_file) == EOF) {
+            fprintf(stderr, "Can't close %s\n", file_name);
+            perror(argv[0]);
+            return (EXIT_FAILURE);
         }
     }
 
@@ -58,9 +58,20 @@ int main(int argc, char *argv[])
     // ERRNO в соответсвующую строку.
 
     // Как переводится? Смотрим в исходниках:
-    // Есть два массива, один интовый - в него залазим и по номеру ERRNO берем число
-    // второй масив - массив чаров с кучей \0 и по сути мы просто берем строку от индекса,
+    // Есть два массива, один интовый - в него залазим и по номеру ERRNO берем
+    // число
+    // второй масив - массив чаров с кучей \0 и по сути мы просто берем строку
+    // от индекса,
     // который мы получили в прошлом массиве, до следующего сивола \0
 
-    return EXIT_SUCCESS;
+    return (EXIT_SUCCESS);
+}
+
+FILE* open_file(const char* file_name) {
+    FILE* file_for_return = fopen(file_name, "r");
+    if (file_for_return == NULL) {
+        fprintf(stderr, "Can't open %s file.\n", file_name);
+        perror("fopen");
+    }
+    return file_for_return;
 }
